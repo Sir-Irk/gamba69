ff089a1ea5022dec9a8d994b9f9b6ef57215539a
sir-irk <jeremyfmontgomery@gmail.com>
2022-01-20 04:56:28 -0600

    fixed user stock position loading

--- src/utils.ts
+++ src/utils.ts
@@ -376,7 +376,7 @@ export async function load_users(): Promise<void> {
             user.dailyStreak = u.dailyStreak;
             user.charityCollectionTime = u.charityCollectionTime;
             user.highestBones = u.highestBones;
-            if (u.stocks) user.stocks = u.stocks;
+            if (u.stockPositions) user.stocks = u.stockPositions;
             if (u.numHorsesOwned != undefined) user.numHorsesOwned = u.numHorsesOwned;
 
             if (u.gameStats) {
